232667223e05287928dc931ce6016672ccd9bc49
mithsin <hsinbluemoon@gmail.com>
2020-09-19 19:18:15 -0400

    init setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import { Home } from 'Pages';
 
 function App() {
   return (
     <div className="App">
-
+        <Switch>
+          <Route exact path="/" component = { Home } />
+        </Switch>
     </div>
   );
 }
