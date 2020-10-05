52f1d49e82c9225b0543df80d6bf09b2a15f36ec
mithsin <hsinbluemoon@gmail.com>
2020-10-05 16:59:19 -0400

    update

--- src/Utils/imageSrc.js
+++ src/Utils/imageSrc.js
@@ -1,5 +1,5 @@
 export const imgSrc = [
-    "https://res.cloudinary.com/paf1david/image/upload/v1601424181/pafoxbnb/ox-01_mrmq7u.jpg",
+    "https://res.cloudinary.com/paf1david/image/upload/v1601931451/pafoxbnb/11efec50-4ab2-44af-8c4a-d63633dbd4ba_ubdae5.jpg",
     "https://res.cloudinary.com/paf1david/image/upload/v1601424181/pafoxbnb/ox-06_ajsm6p.jpg",
     "https://res.cloudinary.com/paf1david/image/upload/v1601424181/pafoxbnb/ox-02_kjb5b8.jpg",
     "https://res.cloudinary.com/paf1david/image/upload/v1601424181/pafoxbnb/ox-03_bld4tt.jpg",
